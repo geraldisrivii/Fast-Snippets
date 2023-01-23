5a47cc26a67170c68a107e7f5ce1705e3f640d9d
Alexander Maltsev <aleksander.freelancer@gmail.com>
2023-01-23 18:05:47 +0600

    Worked version but it need to do testing

--- main.py
+++ main.py
@@ -11,13 +11,15 @@ with open('file.txt', encoding='utf-8') as main_file:
     for element in array_str:
         if (element[0] == " "):
             element = '\t' + element[4:]
-            new_array.append(element)
+            new_array.append(element.strip('\n'))
         elif (element[0] == "!"):
             new_array.append("!")
             count_files += 1
         else:
-            new_array.append(element)
-with open(path, encoding='utf-8') as snippets_file:
+            new_array.append(element.strip('\n'))
+        if(i:= element.find('"')):
+            element = element[:i] + "\"" + element[i:]
+with open(path, encoding='utf-8', mode='r+') as snippets_file:
     snippets = json.load(snippets_file)
     finally_array = []
     sub_array = []
@@ -33,7 +35,11 @@ with open(path, encoding='utf-8') as snippets_file:
     print(f"Count of accepted elements: {len(finally_array)}")
     print("Please, input prefixes to all snippets in sort of have added elements. Input prefixeses throught '.'")
     list_prefixes = input().split()
-    dictionary = {}
     for index in range(len(finally_array)):
-        dictionary[list_prefixes[index]] = finally_array[index]
-    print(dictionary)
+        snippets[list_prefixes[index]] = {
+            "prefix": list_prefixes[index],
+            "body": finally_array[index]
+        }
+    print("OK")
+with open(path, encoding='utf-8', mode='w') as last_file:
+    json.dump(snippets,last_file, indent=2)
